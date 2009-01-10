fcc227037a342f69c855f4dfe69090a85abb646f
Will Thomas <willt@.(none)>
2009-01-10 15:53:06 -0500

    Enhancement: Added a very limited WPF GUI.

---
+++ MetaGeta/MetaGeta.GUI/MainWindow.xaml.vb
@@ -0,0 +1,111 @@
+﻿Imports System.IO
+Imports System.Text
+
+Partial Public Class MainWindow
+
+    Private Sub Window1_Loaded(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles Window1.Loaded
+        Dim t As New System.Threading.Thread(AddressOf ImportTagAndWrite)
+        t.Start()
+    End Sub
+
+    Sub ImportTagAndWrite()
+        Dim template = New MetaGeta.DataStore.TVShowDataStoreTemplate()
+        Dim dsm = New DataStoreManager()
+        dsm.NewDataStore("TV Shows", template)
+        Dim ds = dsm.DataStores(0)
+
+        AddTaggingPluginByName(ds, "MetaGeta.MediaInfoPlugin.MediaInfoPlugin, MediaInfoPlugin")
+        AddTaggingPluginByName(ds, "MetaGeta.TVShowPlugin.EducatedGuessImporter, TVShowPlugin")
+        AddTaggingPluginByName(ds, "MetaGeta.TVDBPlugin.TVDBPlugin, TVDBPlugin")
+
+        AddDirectory(ds, "C:\Users\willt\Desktop\ipod\The Venture Brothers s03e01.mp4")
+        RunTaggingPlugins(ds)
+
+        Dim text = ToCsv(ds)
+        Dispatcher.Invoke(Windows.Threading.DispatcherPriority.Normal, Function() TextBox1.Text = text)
+
+        WriteAllTags(ds)
+
+        ds.Close()
+    End Sub
+
+
+    Public Sub AddTaggingPluginByName(ByVal ds As MGDataStore, ByVal assemblyQualifiedTypeName As String)
+        Dim t = Type.GetType(assemblyQualifiedTypeName, True)
+        Dim plugin = CType(Activator.CreateInstance(t), IMGTaggingPlugin)
+        ds.AddTaggingPlugin(plugin)
+    End Sub
+
+    Public Sub AddDirectory(ByVal ds As MGDataStore, ByVal dir As String)
+        If File.Exists(dir) Then
+            ds.NewFile(New Uri(dir))
+        ElseIf Directory.Exists(dir) Then
+            For Each file In Directory.GetFiles(dir)
+                ds.NewFile(New Uri(file))
+            Next
+        Else
+            Throw New Exception(String.Format("Can't find path: {0}", dir))
+        End If
+    End Sub
+
+    Public Sub RunTaggingPlugins(ByVal ds As MGDataStore)
+        ds.RunTaggingPlugins()
+    End Sub
+
+    Public Function ToCsv(ByVal ds As MGDataStore) As String
+        Dim sb As New StringBuilder()
+        sb.AppendLine(Aggregate s In ds.Template.GetDimensionNames() Order By s Into JoinToCsv(s))
+        For Each f In ds
+            sb.AppendLine(Aggregate t In f.Tags() Order By t.Name Into JoinToCsv(t.Value))
+        Next
+        Return sb.ToString()
+    End Function
+
+    Public Sub WriteAllTags(ByVal ds As MGDataStore)
+        For Each file As MGFile In ds
+            WriteTags(file)
+        Next
+    End Sub
+
+    Sub WriteTags(ByVal f As MGFile)
+        WriteAtomicParsleyTag(f.Path, "TVShowName", f.Tags.Item(TVShowDataStoreTemplate.SeriesTitle).Value)
+        WriteAtomicParsleyTag(f.Path, "Album", f.Tags.Item(TVShowDataStoreTemplate.SeriesTitle).Value)
+        WriteAtomicParsleyTag(f.Path, "TVSeasonNum", f.Tags.Item(TVShowDataStoreTemplate.SeasonNumber).Value)
+        WriteAtomicParsleyTag(f.Path, "TVEpisode", f.Tags.Item(TVShowDataStoreTemplate.EpisodeID).Value)
+        WriteAtomicParsleyTag(f.Path, "TVEpisodeNum", f.Tags.Item(TVShowDataStoreTemplate.EpisodeNumber).Value)
+        WriteAtomicParsleyTag(f.Path, "description", f.Tags.Item(TVShowDataStoreTemplate.EpisodeDescription).Value)
+        If f.Tags.Item(TVShowDataStoreTemplate.EpisodeBanner).IsSet Then
+            WriteAtomicParsleyTag(f.Path, "artwork", f.Tags.Item(TVShowDataStoreTemplate.EpisodeBanner).Value)
+        End If
+        WriteAtomicParsleyTag(f.Path, "stik", "TV Show")
+        WriteAtomicParsleyTag(f.Path, "genre", "TV Shows")
+
+        'WriteAtomicParsleyTag(f.Path, "title", f.Tags.Item(TVShowDataStoreTemplate.EpisodeTitle).Value)
+        WriteAtomicParsleyTag(f.Path, "title", String.Format( _
+          "{0} - s{1}e{2} - {3}", _
+          f.Tags.Item(TVShowDataStoreTemplate.SeriesTitle).Value, _
+          f.Tags.Item(TVShowDataStoreTemplate.SeasonNumber).Value, _
+          f.Tags.Item(TVShowDataStoreTemplate.EpisodeNumber).Value, _
+          f.Tags.Item(TVShowDataStoreTemplate.EpisodeTitle).Value _
+            ))
+    End Sub
+
+    Private Sub WriteAtomicParsleyTag(ByVal path As Uri, ByVal apTagName As String, ByVal value As String)
+        Dim p As New Process()
+        p.StartInfo = New ProcessStartInfo(Environment.ExpandEnvironmentVariables("%TOOLS%\AtomicParsley\AtomicParsley.exe"))
+        Dim sb As New StringBuilder()
+
+        sb.AppendFormat(" ""{0}"" ", path.LocalPath)
+        sb.AppendFormat(" --{0} ""{1}"" ", apTagName, value.Replace("""", """"""))
+        sb.Append(" --overWrite ")
+        p.StartInfo.Arguments = sb.ToString()
+
+        p.Start()
+        p.WaitForExit()
+    End Sub
+
+    Private Function CapLength(ByVal s As String) As String
+        Return s.Substring(0, Math.Min(255, s.Length))
+    End Function
+
+End Class
