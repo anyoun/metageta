79904e9f545cb44ca15a90a519e1213660449945
Will Thomas <will306@oneverni.com>
2009-07-22 00:41:04 -0400

    Enhancements to GridView:  1. Added a context menu for files with a placeholder item for properties.  2. Added file commands to convert to iPhone, write MP4 tags, and remove the file.  3. Slightly tweaks the selected row appearance to more closely match the Aero ListView.

--- MetaGeta/MetaGeta.GUI/Views/GridView.xaml.vb
+++ MetaGeta/MetaGeta.GUI/Views/GridView.xaml.vb
@@ -14,6 +14,8 @@ Partial Public Class GridView
         ' This call is required by the Windows Form Designer.
         InitializeComponent()
 
+        Me.DataContext = Me
+
         For Each t In dataStore.Template.GetColumnNames()
             Dim column As New DataGridTextColumn()
             column.Binding = New Binding() With { _
@@ -23,6 +25,58 @@ Partial Public Class GridView
             column.Header = t
             DataGrid1.Columns.Add(column)
         Next
+    End Sub
+
+    Private m_ConvertToIphoneCommand As RelayCommand(Of MGFile)
+    Public ReadOnly Property ConvertToIphoneCommand() As ICommand
+        Get
+            If m_ConvertToIphoneCommand Is Nothing Then
+                m_ConvertToIphoneCommand = New RelayCommand(Of MGFile)(AddressOf ConvertToIphone)
+            End If
+            Return m_ConvertToIphoneCommand
+        End Get
+    End Property
+    Public Sub ConvertToIphone(ByVal param As MGFile)
+        DataStore.DoAction(param, TranscodePlugin.TranscodePlugin.ConvertActionName)
+    End Sub
+
+    Private m_WriteMp4TagsCommand As RelayCommand(Of MGFile)
+    Public ReadOnly Property WriteMp4TagsCommand() As ICommand
+        Get
+            If m_WriteMp4TagsCommand Is Nothing Then
+                m_WriteMp4TagsCommand = New RelayCommand(Of MGFile)(AddressOf WriteMp4Tags)
+            End If
+            Return m_WriteMp4TagsCommand
+        End Get
+    End Property
+    Public Sub WriteMp4Tags(ByVal param As MGFile)
+        DataStore.DoAction(param, TranscodePlugin.Mp4TagWriterPlugin.c_WriteTagsAction)
+    End Sub
+
+    Private m_RemoveFileCommand As RelayCommand(Of MGFile)
+    Public ReadOnly Property RemoveFileCommand() As ICommand
+        Get
+            If m_RemoveFileCommand Is Nothing Then
+                m_RemoveFileCommand = New RelayCommand(Of MGFile)(AddressOf RemoveFile)
+            End If
+            Return m_RemoveFileCommand
+        End Get
+    End Property
+    Public Sub RemoveFile(ByVal param As MGFile)
+        DataStore.RemoveFiles(param.SingleToEnumerable())
+    End Sub
+
+    Private m_ShowPropertiesCommand As RelayCommand(Of MGFile)
+    Public ReadOnly Property ShowPropertiesCommand() As ICommand
+        Get
+            If m_ShowPropertiesCommand Is Nothing Then
+                m_ShowPropertiesCommand = New RelayCommand(Of MGFile)(AddressOf ShowProperties)
+            End If
+            Return m_ShowPropertiesCommand
+        End Get
+    End Property
+    Public Sub ShowProperties(ByVal param As MGFile)
+        MessageBox.Show("Properties")
     End Sub
 
     Public ReadOnly Property DataStore() As MGDataStore
