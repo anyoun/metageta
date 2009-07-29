e1967596b311f2e7d92207507628f8c298f62784
Will Thomas <will306@oneverni.com>
2009-07-28 23:33:52 -0400

    Enhancement to GridView: The right-click actions now work for all selected files, not just the file under the cursor.

--- MetaGeta/MetaGeta.GUI/Helpers/RelayCommand.vb
+++ MetaGeta/MetaGeta.GUI/Helpers/RelayCommand.vb
@@ -3,17 +3,24 @@
 
     Private ReadOnly m_Execute As Action(Of T)
     Private ReadOnly m_CanExecute As Predicate(Of T)
+    Private ReadOnly m_GetParameter As Func(Of T)
 
     Public Sub New(ByVal execute As Action(Of T))
         Me.New(execute, Nothing)
     End Sub
 
     Public Sub New(ByVal execute As Action(Of T), ByVal canExecute As Predicate(Of T))
+        Me.New(execute, canExecute, Nothing)
+    End Sub
+
+    Public Sub New(ByVal execute As Action(Of T), ByVal canExecute As Predicate(Of T), ByVal getParameter As Func(Of T))
         m_Execute = execute
         m_CanExecute = canExecute
+        m_GetParameter = getParameter
     End Sub
 
     Public Function CanExecute(ByVal parameter As Object) As Boolean Implements System.Windows.Input.ICommand.CanExecute
+        parameter = If(parameter Is Nothing, parameter, m_GetParameter())
         Return If(m_CanExecute Is Nothing, True, m_CanExecute(CType(parameter, T)))
     End Function
 
@@ -29,8 +36,8 @@
         End RaiseEvent
     End Event
 
-
     Public Sub Execute(ByVal parameter As Object) Implements System.Windows.Input.ICommand.Execute
+        parameter = If(parameter Is Nothing, parameter, m_GetParameter())
         m_Execute(CType(parameter, T))
     End Sub
 End Class
--- MetaGeta/MetaGeta.GUI/Views/GridView.xaml.vb
+++ MetaGeta/MetaGeta.GUI/Views/GridView.xaml.vb
@@ -7,6 +7,7 @@ Partial Public Class GridView
     Private Shared ReadOnly log As ILog = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType)
 
     Private ReadOnly m_DataStore As MGDataStore
+    Private m_SelectedFiles As IList(Of MGFile)
 
     Public Sub New(ByVal dataStore As MGDataStore)
         m_DataStore = dataStore
@@ -27,60 +28,75 @@ Partial Public Class GridView
         Next
     End Sub
 
-    Private m_ConvertToIphoneCommand As RelayCommand(Of MGFile)
+    Private m_ConvertToIphoneCommand As RelayCommand(Of IList(Of MGFile))
     Public ReadOnly Property ConvertToIphoneCommand() As ICommand
         Get
             If m_ConvertToIphoneCommand Is Nothing Then
-                m_ConvertToIphoneCommand = New RelayCommand(Of MGFile)(AddressOf ConvertToIphone)
+                m_ConvertToIphoneCommand = New RelayCommand(Of IList(Of MGFile))(AddressOf ConvertToIphone, AddressOf IsNonEmpty, AddressOf GetSelectedFiles)
             End If
             Return m_ConvertToIphoneCommand
         End Get
     End Property
-    Public Sub ConvertToIphone(ByVal param As MGFile)
-        DataStore.DoAction(param, TranscodePlugin.TranscodePlugin.ConvertActionName)
+    Public Sub ConvertToIphone(ByVal param As IList(Of MGFile))
+        For Each f In param
+            DataStore.DoAction(f, TranscodePlugin.TranscodePlugin.ConvertActionName)
+        Next
     End Sub
 
-    Private m_WriteMp4TagsCommand As RelayCommand(Of MGFile)
+    Private m_WriteMp4TagsCommand As RelayCommand(Of IList(Of MGFile))
     Public ReadOnly Property WriteMp4TagsCommand() As ICommand
         Get
             If m_WriteMp4TagsCommand Is Nothing Then
-                m_WriteMp4TagsCommand = New RelayCommand(Of MGFile)(AddressOf WriteMp4Tags)
+                m_WriteMp4TagsCommand = New RelayCommand(Of IList(Of MGFile))(AddressOf WriteMp4Tags, AddressOf IsNonEmpty, AddressOf GetSelectedFiles)
             End If
             Return m_WriteMp4TagsCommand
         End Get
     End Property
-    Public Sub WriteMp4Tags(ByVal param As MGFile)
-        DataStore.DoAction(param, TranscodePlugin.Mp4TagWriterPlugin.c_WriteTagsAction)
+    Public Sub WriteMp4Tags(ByVal param As IList(Of MGFile))
+        For Each f In param
+            DataStore.DoAction(f, TranscodePlugin.Mp4TagWriterPlugin.c_WriteTagsAction)
+        Next
     End Sub
 
-    Private m_RemoveFileCommand As RelayCommand(Of MGFile)
+    Private m_RemoveFileCommand As RelayCommand(Of IList(Of MGFile))
     Public ReadOnly Property RemoveFileCommand() As ICommand
         Get
             If m_RemoveFileCommand Is Nothing Then
-                m_RemoveFileCommand = New RelayCommand(Of MGFile)(AddressOf RemoveFile)
+                m_RemoveFileCommand = New RelayCommand(Of IList(Of MGFile))(AddressOf RemoveFile, AddressOf IsNonEmpty, AddressOf GetSelectedFiles)
             End If
             Return m_RemoveFileCommand
         End Get
     End Property
-    Public Sub RemoveFile(ByVal param As MGFile)
-        DataStore.RemoveFiles(param.SingleToEnumerable())
+    Public Sub RemoveFile(ByVal param As IList(Of MGFile))
+        DataStore.RemoveFiles(param)
     End Sub
 
-    Private m_ShowPropertiesCommand As RelayCommand(Of MGFile)
+    Private m_ShowPropertiesCommand As RelayCommand(Of IList(Of MGFile))
     Public ReadOnly Property ShowPropertiesCommand() As ICommand
         Get
             If m_ShowPropertiesCommand Is Nothing Then
-                m_ShowPropertiesCommand = New RelayCommand(Of MGFile)(AddressOf ShowProperties)
+                m_ShowPropertiesCommand = New RelayCommand(Of IList(Of MGFile))(AddressOf ShowProperties, AddressOf IsExactlyOne, AddressOf GetSelectedFiles)
             End If
             Return m_ShowPropertiesCommand
         End Get
     End Property
-    Public Sub ShowProperties(ByVal param As MGFile)
-        Dim win As New FilePropertiesView(param)
+    Public Sub ShowProperties(ByVal param As IList(Of MGFile))
+        Dim win As New FilePropertiesView(param.First())
         PresentationTraceSources.SetTraceLevel(win, PresentationTraceLevel.High)
         win.Owner = Window.GetWindow(Me)
         win.ShowDialog()
     End Sub
+
+    Private Shared Function IsNonEmpty(ByVal files As IList(Of MGFile)) As Boolean
+        Return files IsNot Nothing AndAlso files.Count > 0
+    End Function
+    Private Shared Function IsExactlyOne(ByVal files As IList(Of MGFile)) As Boolean
+        Return files IsNot Nothing AndAlso files.Count = 1
+    End Function
+
+    Public Function GetSelectedFiles() As IList(Of MGFile)
+        Return m_SelectedFiles
+    End Function
 
     Public ReadOnly Property DataStore() As MGDataStore
         Get
@@ -92,4 +108,9 @@ Partial Public Class GridView
         RaiseEvent PropertyChanged(Me, New PropertyChangedEventArgs(name))
     End Sub
     Public Event PropertyChanged(ByVal sender As Object, ByVal e As System.ComponentModel.PropertyChangedEventArgs) Implements System.ComponentModel.INotifyPropertyChanged.PropertyChanged
+
+    Private Sub DataGrid1_SelectedCellsChanged(ByVal sender As System.Object, ByVal e As Microsoft.Windows.Controls.SelectedCellsChangedEventArgs)
+        Dim cells = DataGrid1.SelectedCells.ToArray()
+        m_SelectedFiles = (From cell In cells Select CType(cell.Item, MGFile)).Distinct().ToList()
+    End Sub
 End Class
