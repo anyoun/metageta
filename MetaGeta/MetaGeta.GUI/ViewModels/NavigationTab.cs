3cdb4d08df081d0ef4f370468cd21d12328f8086
Will Thomas <will306@oneverni.com>
2009-11-07 14:28:54 -0500

    Cleanup to NavigationTab: Removed commented-out code.

--- MetaGeta/MetaGeta.GUI/ViewModels/NavigationTab.cs
+++ MetaGeta/MetaGeta.GUI/ViewModels/NavigationTab.cs
@@ -24,45 +24,6 @@ using System.Windows.Media;
 
 namespace MetaGeta.GUI {
     public abstract class NavigationTab : INotifyPropertyChanged {
-        //Private m_Icon As ImageSource
-        //Private m_Caption As String
-        //Private m_Parent As NavigationTabGroupBase
-
-        //Public Property Icon() As ImageSource
-        //    Get
-        //        Return m_Icon
-        //    End Get
-        //    Set(ByVal value As ImageSource)
-        //        If m_Icon IsNot value Then
-        //            m_Icon = value
-        //            OnPropertyChanged("Icon")
-        //        End If
-        //    End Set
-        //End Property
-
-        //Public Property Caption() As String
-        //    Get
-        //        Return m_Caption
-        //    End Get
-        //    Set(ByVal value As String)
-        //        If m_Caption IsNot value Then
-        //            m_Caption = value
-        //            OnPropertyChanged("Caption")
-        //        End If
-        //    End Set
-        //End Property
-
-        //Public Property Parent() As NavigationTabGroupBase
-        //    Get
-        //        Return m_Parent
-        //    End Get
-        //    Set(ByVal value As NavigationTabGroupBase)
-        //        If m_Parent IsNot value Then
-        //            m_Parent = value
-        //            OnPropertyChanged("Parent")
-        //        End If
-        //    End Set
-        //End Property
         public abstract ImageSource Icon { get; }
         public abstract string Caption { get; }
 
