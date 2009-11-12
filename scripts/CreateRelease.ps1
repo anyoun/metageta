2ff782857040a9f97201d24ef7d4afa801a37623
Will Thomas <will306@oneverni.com>
2009-11-11 23:00:10 -0500

    Fix to build script:  1. Fixed paths to ffmpeg and AtomicParsley.  2. msbuild no longer has to be in the path.

--- scripts/CreateRelease.ps1
+++ scripts/CreateRelease.ps1
@@ -2,9 +2,9 @@ $ErrorActionPreference = "Stop"
 
 function create-directory($dir) {
 	if(-not (test-path $dir)) {
-		(mkdir $dir).FullName + '\'
+		(mkdir $dir).FullName + '\' #'
 	} else {
-		join-path (resolve-path $dir) '\'
+		join-path (resolve-path $dir) '\' #'
 	}
 }
 
@@ -16,10 +16,16 @@ function recreate-directory($dir) {
 	create-directory $dir
 }
 
-if(test-path hkcu:\Software\7-Zip) {
+if(test-path "hkcu:\Software\7-Zip") {
 	$7zipPath = join-path (gi hkcu:\Software\7-Zip).GetValue("Path") "7z.exe"
 } else {
 	throw "No 7zip!"
+}
+
+if(test-path "$($env:windir)\Microsoft.NET\Framework64\v3.5") {
+	$msbuildPath = "$($env:windir)\Microsoft.NET\Framework64\v3.5\msbuild.exe"
+} else {
+	throw "No msbuild!"
 }
 
 $timestamp = (get-date).ToString("yyyyMMdd")
@@ -29,13 +35,13 @@ pushd ..
 [void](create-directory "output")
 $dir = recreate-directory "output\MetaGeta.$timestamp"
 
-msbuild MetaGeta\MetaGeta.sln /v:q /t:Rebuild "/p:Configuration=Release" "/p:Platform=Any CPU" "/p:OutDir=$dir;OutputPath=$dir"
+& $msbuildPath MetaGeta\MetaGeta.sln /v:q /t:Rebuild "/p:Configuration=Release" "/p:Platform=Any CPU" "/p:OutDir=$dir;OutputPath=$dir"
 
 pushd $dir
 	rm *.xml
 	rm *.pdb
-	copy ..\tools\AtomicParsley\AtomicParsley.exe .
-	copy ..\tools\ffmpeg\ffmpeg.exe .
+	copy ..\..\tools\AtomicParsley\AtomicParsley.exe .
+	copy ..\..\tools\ffmpeg\ffmpeg.exe .
 popd
 
 & $7zipPath a -mx9 -mmt "output\MetaGeta.$timestamp.7z" "$dir"
